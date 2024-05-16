0f08a797c7d330183ace28e59673f47fdf2701bd
Paulo Henrique de Andrade <phandradefilho@hotmail.com>
2024-05-16 19:41:53 -0300

    Testando servidor

--- index.js
+++ index.js
@@ -1,23 +1,39 @@
 const { createApp } = Vue;
-const API_URL = 'http://localhost:3000';
-
+const API_URL = 'https://jogobd.onrender.com';
 createApp({
     data() {
         return {
             heroi: { vida: 100 },
-            vilao: { vida: 100 }            
-        };        
+            vilao: { vida: 100 },
+            aux: 1,
+            mensagemAcao: '',
+            historico: [],
+            acao: ''
+        }
+        
     },
     methods: {
         atacar(isHeroi) {
+            let acao = isHeroi ? "Herói atacou" : "Vilão atacou";
             if (isHeroi) {
-                this.vilao.vida -= 10;
-                this.atualizarVidaNoBancoDeDados(this.heroi.vida, this.vilao.vida);
+                this.vilao.vida = this.vilao.vida > 5 ? this.vilao.vida - 10 : 0;
+                this.historico.push("Herói atacou. Vida do vilão: " + this.vilao.vida);
+                this.acao = "Herói atacou"; 
+                console.log("Herói atacou");
                 this.acaoVilao();
+                this.aux = 1;
+                this.atualizarVidaNoBancoDeDados(this.heroi.vida, this.vilao.vida);
+                
             } else {
-                this.heroi.vida -= 20;
+                this.mensagemAcao = "Vilão atacou"; 
+                this.historico.push("Vilão atacou. Vida do herói: " + this.heroi.vida);
+                this.historico.acao = "Vilão atacou"; 
+                console.log("Vilão atacou");
+                this.heroi.vida = this.heroi.vida > 5 ? this.heroi.vida - 10 : 0;
                 this.atualizarVidaNoBancoDeDados(this.vilao.vida, this.heroi.vida);
             }
+            this.adicionarHistorico(acao);
+            this.verificarVida();
         },
         async atualizarVidaNoBancoDeDados(vidaHeroi, vidaVilao) {
             try {
@@ -37,27 +53,105 @@ createApp({
             }
         },
         defender(isHeroi) {
-            this.acaoVilao();
+            let acao = isHeroi ? "Herói defendeu" : "Vilão defendeu";
+            if (isHeroi) {
+                console.log("Herói defendeu");
+                this.acaoVilao();
+                this.historico.push("Herói defendeu.");
+                this.acao = "Herói defendeu"; 
+            } else {
+                this.mensagemAcao = "Vilão defendeu"; 
+                console.log("Vilão defendeu");
+                this.aux = 1; 
+                this.historico.push("Vilão defendeu.");
+                this.acao = "Vilão defendeu"; 
+            }
+            this.adicionarHistorico(acao);
+            this.verificarVida();
         },
         usarPocao(isHeroi) {
+            let acao = isHeroi ? "Herói usou poção" : "Vilão usou poção";
             if (isHeroi) {
-                this.vilao.vida += 10;
-                this.atualizarVidaNoBancoDeDados(this.heroi.vida, this.vilao.vida);
+                this.acao = "Herói usou poção"; 
+                console.log("Herói usou poção");
+                this.heroi.vida = this.heroi.vida < 95 ? this.heroi.vida + 5 : 100;
+                this.aux = 1;
                 this.acaoVilao();
+                this.historico.push("Herói usou poção. Vida do herói: " + this.heroi.vida);
+                this.atualizarVidaNoBancoDeDados(this.heroi.vida, this.vilao.vida);
             } else {
-                this.heroi.vida += 10;
+                this.mensagemAcao = "Vilão usou poção"; 
+                this.acao = "Vilão usou poção"; 
+                console.log("Vilão usou poção");
+                this.vilao.vida = this.vilao.vida < 95 ? this.vilao.vida + 5 : 100;
+                this.historico.push("Vilão usou poção. Vida do vilão: " + this.vilao.vida);
                 this.atualizarVidaNoBancoDeDados(this.vilao.vida, this.heroi.vida);
             }
-            this.acaoVilao();
+            this.adicionarHistorico(acao);
+            this.verificarVida();
         },
-        correr(isHeroi) {
-            this.acaoVilao();
+        correr(heroiCorrendo) { 
+            let acao = heroiCorrendo ? "Herói correu" : "Vilão correu";
+            if (heroiCorrendo) {
+                alert("Fim de Jogo: VILÃO GANHOU");
+                console.log("Herói correu");
+                this.historico.push("Herói correu.");
+                this.acao = "Herói correu"; 
+                
+            } else {
+                this.mensagemAcao = "Vilão correu"; 
+                alert("Fim de Jogo: HERÓI GANHOU");
+                console.log("Vilão correu");
+                this.historico.push("Vilão correu.");
+                this.acao = "Vilão correu"; 
+            }  this.adicionarHistorico(acao);
+        },
+        verificarVida() {
+            if (this.heroi.vida <= 0) {
+                alert("Fim de Jogo: VILÃO GANHOU");
+                location.reload()
+            } else if (this.vilao.vida <= 0) {
+                alert("Fim de Jogo: HERÓI GANHOU");
+                location.reload()
+            }
+
+        },
+        vidaCor(vida) {
+            if (vida <= 35) {
+                return 'baixa';
+            } else if (vida <= 60) {
+                return 'media';
+            } else {
+                return 'alta';
+            }
         },
         acaoVilao() {
-            const acoes = ['atacar', 'defender', 'usarPocao', 'correr'];
+            const acoes = ['atacar', 'atacar', 'atacar', 'atacar', 'atacar', // probabilidade de ocorrer a ação 50%
+            'defender', 'defender', // 20%
+            'usarPocao', 'usarPocao', // 20%
+            'correr' // 10%
+            ];
             const acaoAleatoria = acoes[Math.floor(Math.random() * acoes.length)];
             this[acaoAleatoria](false);
-            console.log('O vilão usou: ' + acaoAleatoria)
+            
+        },
+        async adicionarHistorico(acao) {
+            try {
+                //let acao = this.acao;
+                const response = await fetch(`${API_URL}/adicionarHistorico`, {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({acao})
+                });
+                if (!response.ok) {
+                    throw new Error('Erro ao atualizar o historico no banco de dados.');
+                }
+                console.log('Historico atualizados com sucesso.');
+            } catch (error) {
+                console.error('Erro ao atualizar o historico no banco de dados:', error);
+            }
         }
     }
 }).mount("#app");
--- server.js
+++ server.js
@@ -2,15 +2,24 @@ const express = require('express');
 const path = require('path');
 const sql = require('mssql');
 
+
 const app = express();
 const PORT = 3000;
 
+// Middleware para habilitar o CORS
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*'); // Permitindo acesso de qualquer origem
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE'); // Métodos permitidos
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization'); // Cabeçalhos permitidos
+    next();
+});
+
 // Configuração do banco de dados
 const config = {
-    user: 'pblcnr',
+    user: "pblcnr",
     password: 'A7xolimpo1442!',
-    server: 'brbr-server.database.windows.net',
-    database: 'bd-heroivilao',
+    server: "brbr-server.database.windows.net",
+    database: "bd-heroivilao",
     options: {
         encrypt: true // Dependendo da configuração do seu servidor SQL Server
     }
@@ -21,29 +30,100 @@ app.use(express.json());
 // Servir arquivos estáticos (como index.html)
 app.use(express.static(path.join(__dirname)));
 
+// Limpar a tabela Historico quando o servidor é iniciado
+async function limparHistorico() {
+    try {
+        const pool = await sql.connect(config);
+        const request = pool.request();
+        await request.query("DELETE FROM Historico");
+        console.log('Tabela Historico limpa com sucesso.');
+    } catch (error) {
+        console.error('Erro ao limpar a tabela Historico:', error);
+    }
+}
+// Chamar a função para limpar a tabela Historico quando o servidor é iniciado
+limparHistorico();
 // Rota para atualizar a vida do herói e do vilão
 app.post('/atualizarVida', async (req, res) => {
     const { vidaHeroi, vidaVilao } = req.body;
 
-    try {
-        await sql.connect(config);
-        const request = new sql.Request();
-        await request.query(`
-      MERGE INTO Personagens AS target
-      USING (VALUES ('heroi', ${vidaHeroi}), ('vilao', ${vidaVilao})) AS source (Nome, Vida)
-      ON target.Nome = source.Nome
-      WHEN MATCHED THEN
-        UPDATE SET Vida = source.Vida
-      WHEN NOT MATCHED THEN
-        INSERT (Nome, Vida) VALUES (source.Nome, source.Vida);
-      `);
-        res.status(200).send('Vida do herói e do vilão atualizada com sucesso.');
-    } catch (err) {
-        console.error(err);
-        res.status(500).send('Erro ao atualizar a vida do herói e do vilão.');
+    // Verifica se vidaHeroi e vidaVilao estão definidos e são números válidos
+    if (typeof vidaHeroi !== 'undefined' && typeof vidaVilao !== 'undefined' && !isNaN(vidaHeroi) && !isNaN(vidaVilao)) {
+        try {
+            await sql.connect(config);
+            const request = new sql.Request();
+            await request.query(`
+                MERGE INTO Personagens AS target
+                USING (VALUES ('heroi', ${vidaHeroi}), ('vilao', ${vidaVilao})) AS source (Nome, Vida)
+                ON target.Nome = source.Nome
+                WHEN MATCHED THEN
+                    UPDATE SET Vida = source.Vida
+                WHEN NOT MATCHED THEN
+                    INSERT (Nome, Vida) VALUES (source.Nome, source.Vida);
+            `);
+            res.status(200).send('Vida do herói e do vilão atualizada com sucesso.');
+        } catch (err) {
+            console.error('Erro ao atualizar a vida do herói e do vilão:', err);
+            res.status(500).send('Erro ao atualizar a vida do herói e do vilão.');
+        }
+    } else {
+        // Caso vidaHeroi ou vidaVilao sejam indefinidos ou não sejam números válidos
+        console.error('Erro ao atualizar a vida do herói e do vilão: vidaHeroi ou vidaVilao estão indefinidos ou não são números válidos.');
+        res.status(400).send('Erro ao atualizar a vida do herói e do vilão: vidaHeroi ou vidaVilao estão indefinidos ou não são números válidos.');
+    }
+});
+// Rota para atualizar os dados do usuário
+app.post('/atualizarUsuario', async (req, res) => {
+    const { nome, email, senha } = req.body;
+
+    // Verifica se nome, email e senha estão definidos
+    if (nome && email && senha) {
+        try {
+            await sql.connect(config);
+            const request = new sql.Request();
+            await request.query(`
+    MERGE INTO usuario AS target
+    USING (VALUES ('${nome}', '${email}', '${senha}')) AS source (nome, email, senha)
+    ON target.email = source.email
+    WHEN MATCHED THEN
+        UPDATE SET nome = source.nome, senha = source.senha
+    WHEN NOT MATCHED THEN
+        INSERT (nome, email, senha) VALUES (source.nome, source.email, source.senha);
+`);
+
+            res.status(200).send('Dados do usuário atualizados com sucesso.');
+        } catch (err) {
+            console.error('Erro ao atualizar os dados do usuário:', err);
+            res.status(500).send('Erro ao atualizar os dados do usuário.');
+        }
+    } else {
+        // Caso nome, email ou senha sejam indefinidos
+        console.error('Erro ao atualizar os dados do usuário: nome, email ou senha estão indefinidos.');
+        res.status(400).send('Erro ao atualizar os dados do usuário: nome, email ou senha estão indefinidos.');
     }
 });
 
+// Rota para adicionar uma entrada na tabela de histórico
+app.post('/adicionarHistorico', async (req, res) => {
+    const { acao } = req.body;
+
+    if (typeof acao !== 'undefined') {
+        try {
+            await sql.connect(config);
+            const request = new sql.Request();
+            await request.query(`
+                INSERT INTO Historico (acao) VALUES ('${acao}');
+            `);
+            res.status(200).send('Ação adicionada ao histórico com sucesso.');
+        } catch (err) {
+            console.error('Erro ao adicionar ação ao histórico:', err);
+            res.status(500).send('Erro ao adicionar ação ao histórico.');
+        }
+    } else {
+        console.error('Erro ao adicionar ação ao histórico: ação está indefinida.');
+        res.status(400).send('Erro ao adicionar ação ao histórico: ação está indefinida.');
+    }
+});
 // Rota para fornecer os dados do herói e do vilão
 app.get('/characters', async (req, res) => {
     try {
@@ -64,14 +144,61 @@ app.get('/characters', async (req, res) => {
         res.status(500).json({ error: 'Erro ao buscar dados do herói e do vilão.' });
     }
 });
+// Rota para fornecer os dados do usuário
+app.post('/usuario', async (req, res) => {
+    const { nome, senha } = req.body;
+
+    try {
+        await sql.connect(config);
+        const request = new sql.Request();
+
+        // Consulta para obter os dados do usuário com base no nome e senha fornecidos
+        const userResult = await request.query(`SELECT * FROM usuario WHERE nome = '${nome}' AND senha = '${senha}'`);
+        const usuario = userResult.recordset[0];
+
+        if (usuario) {
+            res.status(200).send('Login bem-sucedido.');
+        } else {
+            res.status(401).send('Credenciais inválidas.');
+        }
+    } catch (error) {
+        console.error('Erro ao buscar dados do usuário:', error);
+        res.status(500).json({ error: 'Erro ao buscar dados do usuário.' });
+    }
+});
+
+// Rota para obter o histórico de ações
+app.get('/historico', async (req, res) => {
+    try {
+        await sql.connect(config);
+        const request = new sql.Request();
+
+        const result = await request.query("SELECT TOP 6 * FROM Historico ORDER BY id DESC ");
+        const historico = result.recordset;
+
+         // Limpar apenas as informações da tabela
+        ///await request.query("DELETE FROM Historico");
+
+        res.json({ historico });
+    } catch (error) {
+        console.error('Erro ao buscar histórico de ações:', error);
+        res.status(500).json({ error: 'Erro ao buscar histórico de ações.' });
+    }
+});
 
 // Rota para servir o arquivo HTML principal
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+    res.sendFile(path.join(__dirname, 'login.html'));
 });
 
 app.get('/dashboard', (req, res) => {
     res.sendFile(path.join(__dirname, 'dashboard.html'));
+});
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, 'login.html'));
+});
+app.get('/index', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 // Iniciar o servidor
